81533f7977dbd35864266b0400d3fd3fc699851e
Rithika-Gurusamy <rithika7rithika@gmail.com>
2026-07-22 21:48:02 +0530

    endpoints with path & query parameters

--- main.py
+++ main.py
@@ -1,4 +1,75 @@
 from fastapi import FastAPI
+
+app = FastAPI()
+
+
+
+@app.get("/")
+def fund():
+    return "hello"
+
+@app.get("/students/{roll}")
+def getroll(roll : int):
+    return {
+        "std _id" : roll
+    }
+
+@app.get("/students")
+def search(name : str ) :
+    return {
+        "student_name" : name
+    }
+
+@app.get("/books")
+def getbook(year : int ):
+    return {
+    "year": year
+}
+
+@app.get("/employees")
+def getemployees(limit : int):
+    return {
+    "limit": limit
+}
+
+@app.get("/movies")
+def getmovie(genre : str , year : int):
+    return {
+    "genre" : genre,
+    "year": year
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
@@ -11,3 +82,4 @@ def fun(book_id):
     return {
         "book_id" : book_id
     }
+    '''
