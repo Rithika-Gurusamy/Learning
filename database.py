7c350f286a9a4a9ad9a38dcdc4ea7bd9991d5c68
RITHIKA G <rithika7rithika@gmail.com>
2026-08-20 16:30:54 +0530

    sqlalchemy text,result objects

--- database.py
+++ database.py
@@ -1,5 +1,7 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine,text
 DATABASE_URL = "postgresql://postgres:peaceandpeace@localhost:5432/my_practice_db"
 engine = create_engine(DATABASE_URL)
 with engine.connect() as connection:
-    print("database connected")
+   res = connection.execute(text("SELECT 2+2"))
+   print(res.fetchone()[0])
+
