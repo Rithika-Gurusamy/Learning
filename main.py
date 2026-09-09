64e0cae7ffd5727546ed7d03417de8303039fd6b
RITHIKA G <rithika7rithika@gmail.com>
2026-09-09 16:12:39 +0530

    structuring-2-API Router

--- main.py
+++ main.py
@@ -1,97 +1,12 @@
-from fastapi import FastAPI,HTTPException,Depends
-from database import SessionLocal,get_db
-from models import Student
-from sqlalchemy import select
-from sqlalchemy.orm import Session
-from schemas.student import StudentCreate , StudentUpdate , Studentresponse
-
+from fastapi import FastAPI
+from routers.student import student_router
 app = FastAPI()
+app.include_router(student_router)
 
 
 
 
 
-
-@app.post("/student")
-def create_student(student: StudentCreate,db:Session = Depends(get_db)):
-
-    
-        db_student = Student(
-            name=student.name,
-            roll=student.roll,
-            email=student.email,
-            age=student.age
-        )
-
-        db.add(db_student)
-
-        db.commit()
-
-        return {
-            "message": "Student created successfully"
-        }
-
-@app.get("/students")
-def get_students(db:Session = Depends(get_db)):
-
-    
-        stmt = select(Student)
-
-        result = db.execute(stmt)
-
-        students = result.scalars().all()
-
-        return students
-
-@app.get("/students/{roll}")
-def get_student_by_roll(roll : int,db:Session = Depends(get_db)):
-
-        stmt = select(Student).where(Student.roll == roll)
-
-        result = db.execute(stmt)
-
-        st = result.scalar_one_or_none()
-        if st == None:
-            raise HTTPException(status_code=404 , detail = "student not found")
-        return st
-
-@app.get("/students/email/{email}")
-def get_student_by_email(email : str,db:Session = Depends(get_db)):
-    
-        stmt = select(Student).where(Student.email == email)
-        result = db.execute(stmt)
-        st = result.scalars().all()
-        if len(st) > 0:
-            return st
-        else:
-            raise HTTPException(status_code=404 , detail = "student not found")
-
-@app.patch("/student/update/{roll}",response_model = Studentresponse)
-def updatestudent(roll : int ,st : StudentUpdate,db:Session = Depends(get_db)):
-        
-        stmt = select(Student).where(Student.roll == roll)
-        res = db.execute(stmt)
-        s = res.scalar_one_or_none()
-        if s == None:
-            raise HTTPException(status_code = 404 , detail = "student not found")
-        else:
-            updatedata = st.model_dump(exclude_unset = True)
-            for key,value in updatedata.items():
-                setattr(s,key,value)
-            db.commit()
-            db.refresh(s)
-            return s 
-@app.delete("/student/delete/{roll}")
-def student_delete(roll : int , db:Session = Depends(get_db)):
-        stmt = select(Student).where(Student.roll == roll)
-        res = db.execute(stmt)
-        s = res.scalar_one_or_none()
-        if s == None:
-            raise HTTPException(status_code=404 , detail = "student not found")
-        else:
-            db.delete(s)
-            db.commit()
-            return {"message" : "Student deleted successfully"}
 '''
 students = []
 class studentresponse(BaseModel):
