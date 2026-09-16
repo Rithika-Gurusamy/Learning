376d4c30a6d0adc162592e59ec9aa104ffe03cf8
RITHIKA G <rithika7rithika@gmail.com>
2026-09-16 11:19:41 +0530

    login-verify

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from routers.student import student_router
+from routers.auth import auth_router
 app = FastAPI()
 app.include_router(student_router)
-
+app.include_router(auth_router)
 
 
 
--- routers/auth.py
+++ routers/auth.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter,HTTPException,Depends
 from models import User
 from database import get_db
+from sqlalchemy import select
 from schemas.user import UserCreate
 from sqlalchemy.orm import Session
-from security import hash_password
+from security import hash_password,verify_password
 
 
 auth_router = APIRouter(
@@ -29,4 +30,20 @@ def user_signup(user:UserCreate,db:Session = Depends(get_db)):
         "message": f"user {new_user.username} registered successfully"
     }
     
-    
+@auth_router.post("/login")
+def user_login(user:UserCreate,db:Session = Depends(get_db)):
+
+    n = user.username 
+    stmt = select(User).where(User.username == n)
+    res = db.execute(stmt)
+    us = res.scalar_one_or_none()
+    if us == None:
+        raise HTTPException(status_code =404 , detail = "user not found")
+    else:
+        if verify_password(user.password,us.hashed_password):
+            return {
+                "message" : "user logged in successfully"
+            }
+        else:
+            raise HTTPException(status_code = 401 , detail = "invalid password")
+        
