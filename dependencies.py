07f4b73ec45891ed096287bad6f8fd85e409c0be
RITHIKA G <rithika7rithika@gmail.com>
2026-09-18 12:25:55 +0530

    token decoding

---
+++ dependencies.py
@@ -0,0 +1,49 @@
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+from jose import jwt, JWTError
+
+from database import get_db
+from models import User
+from security import SECRET_KEY, ALGORITHM
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)):
+
+    try:
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM]
+        )
+
+        user_id = payload.get("sub")
+        if user_id is None:
+            raise HTTPException(
+                status_code=401,
+                detail="Invalid token"
+            )
+
+    except JWTError:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid or expired token"
+        )
+
+    stmt = select(User).where(User.id == int(user_id))
+
+    result = db.execute(stmt)
+
+    current_user = result.scalar_one_or_none()
+
+    if current_user is None:
+        raise HTTPException(
+            status_code=401,
+            detail="User not found"
+        )
+
+    return current_user
