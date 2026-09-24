205b025e1f98e4e31f41e601fe5cbb437bdad5a8
RITHIKA G <rithika7rithika@gmail.com>
2026-09-24 10:54:35 +0530

    add object level authorization to all

--- routers/student.py
+++ routers/student.py
@@ -12,7 +12,33 @@ student_router = APIRouter(
     tags=["Students"]
 )
 
+@student_router.get("")
+def get_students(db:Session = Depends(get_db),current_user : User = Depends(get_current_user)):
 
+    
+        stmt = select(Student)
+        result = db.execute(stmt)
+        students = result.scalars().all()
+        ids = []
+        for s in students:
+            ids.append(s.user_id)
+        if current_user.id in ids:
+            return students
+        else:
+            raise HTTPException(status_code = 403 , detail = "you are not authorized to access thisS")
+
+@student_router.get("/email/{email}")
+def get_student_by_email(email : str,db:Session = Depends(get_db),current_user : User = Depends(get_current_user)):
+    
+        stmt = select(Student).where(Student.email == email)
+        result = db.execute(stmt)
+        st = result.scalars().all()
+        if st and st.user_id == current_user.id:
+            return st
+        elif len(st) <= 0:
+            raise HTTPException(status_code=404 , detail = "student not found")
+        else:
+            raise HTTPException(status_code=403 , detail = "you are not authorized to access this student")
 
 @student_router.post("")
 def create_student(student: StudentCreate,db:Session = Depends(get_db),current_user : User = Depends(get_current_user)):
@@ -34,17 +60,7 @@ def create_student(student: StudentCreate,db:Session = Depends(get_db),current_u
             "message": "Student created successfully"
         }
 
-@student_router.get("")
-def get_students(db:Session = Depends(get_db)):
 
-    
-        stmt = select(Student)
-
-        result = db.execute(stmt)
-
-        students = result.scalars().all()
-
-        return students
 
 @student_router.get("/{roll}")
 def get_student_by_roll(roll : int,db:Session = Depends(get_db),current_user : User = Depends(get_current_user)):
@@ -60,40 +76,35 @@ def get_student_by_roll(roll : int,db:Session = Depends(get_db),current_user : U
             raise HTTPException(status_code=403 , detail = "you are not authorized to access this student")
        
 
-@student_router.get("/email/{email}")
-def get_student_by_email(email : str,db:Session = Depends(get_db)):
-    
-        stmt = select(Student).where(Student.email == email)
-        result = db.execute(stmt)
-        st = result.scalars().all()
-        if len(st) > 0:
-            return st
-        else:
-            raise HTTPException(status_code=404 , detail = "student not found")
 
 @student_router.patch("/{roll}",response_model = Studentresponse)
-def updatestudent(roll : int ,st : StudentUpdate,db:Session = Depends(get_db)):
+def updatestudent(roll : int ,st : StudentUpdate,db:Session = Depends(get_db),current_user : User = Depends(get_current_user)):
         
         stmt = select(Student).where(Student.roll == roll)
         res = db.execute(stmt)
         s = res.scalar_one_or_none()
         if s == None:
             raise HTTPException(status_code = 404 , detail = "student not found")
-        else:
+        elif s.user_id == current_user.id:
             updatedata = st.model_dump(exclude_unset = True)
             for key,value in updatedata.items():
                 setattr(s,key,value)
             db.commit()
             db.refresh(s)
             return s 
+        else:
+            raise HTTPException(status_code=403 , detail = "you are not authorized to update this student")
+
 @student_router.delete("/{roll}")
-def student_delete(roll : int , db:Session = Depends(get_db)):
+def student_delete(roll : int , db:Session = Depends(get_db),current_user : User = Depends(get_current_user)):
         stmt = select(Student).where(Student.roll == roll)
         res = db.execute(stmt)
         s = res.scalars().one_or_none()
         if s == None:
             raise HTTPException(status_code=404 , detail = "student not found")
-        else:
+        elif s.user_id == current_user.id:
             db.delete(s)
             db.commit()
             return {"message" : "Student deleted successfully"}
+        else:
+            raise HTTPException(status_code=403 , detail = "you are not authorized to delete this student")
